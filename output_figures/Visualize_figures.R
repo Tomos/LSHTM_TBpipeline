9c825079687c8830c5a34076cd21ec0bdcad5c99
Tomos <tomos.pj@btinternet.com>
2023-07-19 22:22:58 +0100

    Output figures

--- output_figures/Visualize_figures.R
+++ output_figures/Visualize_figures.R
@@ -10,22 +10,28 @@ base_dir <- "C:/Users/tomos/OneDrive/TB_pipeline/output_figures/"
 
 Figure2A <- readRDS(paste0(base_dir, "Figure_2A.rds"))
 plot(Figure2A)
+ggsave(paste0(base_dir, "Figure2A.png"), Figure2A, width = 8, height = 5)
 
 Figure2A_mod <- readRDS(paste0(base_dir, "Figure_2A_mod.rds"))
 plot(Figure2A_mod)
+ggsave(paste0(base_dir, "Figure2A_mod.png"), Figure2A_mod, width = 8, height = 5)
 
 ### Figure 2:
 
 Figure2 <- readRDS(paste0(base_dir, "Figure_2.rds"))
 plot(Figure2)
+ggsave(paste0(base_dir, "Figure2.png"), Figure2, width = 8, height = 5)
 
 Figure2_mod <- readRDS(paste0(base_dir, "Figure_2_mod.rds"))
 plot(Figure2_mod)
+ggsave(paste0(base_dir, "Figure2_mod.png"), Figure2_mod, width = 8, height = 5)
 
 ### Figure 3:
 
 Figure3 <- readRDS(paste0(base_dir, "Figure_3.rds"))
 plot(Figure3)
+ggsave(paste0(base_dir, "Figure3.png"), Figure3, width = 8, height = 5)
 
 Figure3_mod <- readRDS(paste0(base_dir, "Figure_3_mod.rds"))
 plot(Figure3_mod)
+ggsave(paste0(base_dir, "Figure3_mod.png"), Figure3_mod, width = 8, height = 5)
